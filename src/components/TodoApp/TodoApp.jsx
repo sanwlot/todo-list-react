894283cef7227d02cfd96d750b29212564052e39
sunnySavlot <sunny.savlot@gmail.com>
2024-03-25 13:18:57 +0530

    edit todo feature added

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -1,129 +1,172 @@
-import './TodoApp.css'
-import { useEffect, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid';
+import "./TodoApp.css";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoApp() {
-  const [task, setTask] = useState('')
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('todoList')) || []) 
+  const [task, setTask] = useState("");
+  const [editTask, setEditTask] = useState("");
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("todoList")) || []
+  );
 
   useEffect(() => {
-    localStorage.setItem('todoList', JSON.stringify(todoList))
-  }, [todoList])
-
-  function handleInputChange(e) {
-    setTask(e.target.value)
-  }
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }, [todoList]);
 
   function addTodo() {
     if (task) {
-      setTodoList(prevTodoList => {
-        return [...prevTodoList, {id: uuidv4(), task, isFinished: false}]
-      })
-      setTask('')
+      setTodoList((prevTodoList) => {
+        return [
+          ...prevTodoList,
+          { id: uuidv4(), task, isFinished: false, editInput: false },
+        ];
+      });
+      setTask("");
     }
   }
 
   function addTodoOnEnterKey(e) {
-    if (e.key === 'Enter' || e.keyCode === 13 || e.which === 13) {
-      addTodo()
+    if (e.key === "Enter" || e.keyCode === 13 || e.which === 13) {
+      addTodo();
     }
   }
 
   function deleteTodo(id) {
-    setTodoList(prevTodoList => {
-      return prevTodoList.filter( todo => {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.filter((todo) => {
         if (todo.id !== id) {
-          return todo
+          return todo;
         }
-      })
-    })
+      });
+    });
   }
 
-  function editTodo(id) {
-    if (task) {
-      setTodoList(prevTodoList => {
-        return prevTodoList.map(todo => {
-          if (id === todo.id) {
-            return {
-              ...todo,
-              task
-            }
-          } else {
-            return todo
-          }
-        })
-      })
-      setTask('')
-    }
-  }
-
-  function handleCheckBox(id) {
-   setTodoList(prevTodoList => {
-      return prevTodoList.map(todo => {
+  function toggleLineThrough(id) {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.map((todo) => {
         if (todo.id === id) {
           return {
             ...todo,
-            isFinished: !todo.isFinished
-          }
+            isFinished: !todo.isFinished,
+          };
         } else {
-          return todo
+          return todo;
         }
-      })
-   })
+      });
+    });
   }
 
   function clearAllTodos() {
-    setTodoList([])
+    setTodoList([]);
   }
 
-  const todosElements = todoList.map(todo => {
+  function toggleEditTodo(id) {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            editInput: !todo.editInput,
+          };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function saveEditedTodo(id) {
+    if (editTask) {
+      setTodoList((prevTodoList) => {
+        return prevTodoList.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              task: editTask,
+              editInput: false,
+            };
+          }
+          return todo;
+        });
+      });
+      setEditTask("");
+    }
+  }
+
+  function saveEditedTodoOnEnterKey(e, id) {
+    if (e.key === "Enter" || e.keyCode === 13 || e.which === 13) {
+      saveEditedTodo(id);
+    }
+  }
+
+  const todosElements = todoList.map((todo) => {
     return (
-      <div key={todo.id} className='todo-task-element'>
-        <input 
-          className='checkbox'
-          type="checkbox"
-          onChange={()=>handleCheckBox(todo.id)}
-          value={todo.isFinished}
-        />
-        <label 
-          style={{textDecoration: todo.isFinished ? "line-through" : ""}}
-        >
-          {todo.task}
-        </label> 
-        <button onClick={()=>editTodo(todo.id)} className='todo-btn' >Edit</button> 
-        <button onClick={()=>deleteTodo(todo.id)} className='todo-btn' >Delete</button> 
+      <div key={todo.id} className="todo-task-element">
+        <div className="todo-task">
+          <div
+            className="todo-item"
+            onClick={() => toggleLineThrough(todo.id)}
+            style={{ textDecoration: todo.isFinished ? "line-through" : "" }}
+          >
+            {!todo.editInput && todo.task}
+            {todo.editInput && (
+              <div>
+                <input
+                  type="text"
+                  placeholder="edit task..."
+                  className="edit-todo-input"
+                  onChange={(e) => setEditTask(e.target.value)}
+                  onKeyDown={(e) => saveEditedTodoOnEnterKey(e, todo.id)}
+                  value={editTask}
+                />
+                <button
+                  className="save-edit-btn"
+                  onClick={() => saveEditedTodo(todo.id)}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="todo-btns-del-edit">
+          <button
+            onClick={() => toggleEditTodo(todo.id)}
+            className="todo-btn edit-btn"
+          >
+            {todo.editInput ? "Cancel" : "Edit"}
+          </button>
+          <button
+            onClick={() => deleteTodo(todo.id)}
+            className="todo-btn del-btn"
+          >
+            Delete
+          </button>
+        </div>
       </div>
-    )
-  })
+    );
+  });
 
   return (
-    <main className='todo-app'>
-        <div>
-          <input 
-            type="text" 
-            className='todo-input'
-            placeholder='enter tasks'
-            name='task'
-            onChange={handleInputChange}
-            onKeyDown={addTodoOnEnterKey}
-            value={task}
-          />
-          <button
-            onClick={addTodo}
-            className='btn-enter'
-          >
-            Enter
-          </button>
-          <button
-            onClick={clearAllTodos}
-            className='btn-clear'
-          >
-            Clear All
-          </button>
-        </div>
-        <div>
-          {todosElements}
-        </div>
+    <main className="todo-app">
+      <div>
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="enter tasks..."
+          name="task"
+          onChange={(e) => setTask(e.target.value)}
+          onKeyDown={addTodoOnEnterKey}
+          value={task}
+        />
+        <button onClick={addTodo} className="btn-enter">
+          Enter
+        </button>
+        <button onClick={clearAllTodos} className="btn-clear">
+          Clear All
+        </button>
+      </div>
+      <div className="todo-list">{todosElements}</div>
     </main>
-  )
+  );
 }
